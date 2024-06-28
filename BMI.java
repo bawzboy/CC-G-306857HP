08491fe2f2f559287a80041d325595caf3cac20a
Helge <helgepeggen@web.de>
2024-06-28 14:26:26 +0200

    Try to use IO.java

--- BMI.java
+++ BMI.java
@@ -1,25 +1,25 @@
-import java.util.Scanner;
+//import java.util.Scanner;
 
 public class BMI {
 
     public static void main(String[] args) {
 		
-        Scanner scanner = new Scanner(System.in);
+        //Scanner scanner = new Scanner(System.in);
 
-        System.out.println("What's your name? ");
-        String name = scanner.next();
+        String name = IO.promptAndReadString("What's your name? ");
+        //String name = scanner.next();
+        IO.writeln("Hello " + name);
         
-        System.out.println("What's your weight in kilogram? ");
-        Double weight = scanner.nextDouble();  
+        Double weight = IO.promptAndReadDouble("What's your weight in kilogram? ");
+        //Double weight = scanner.nextDouble();  
 
-        System.out.println("What's your height in meter? ");
-        Double height = scanner.nextDouble();
+        Double height = IO.promptAndReadDouble("What's your height in meter? ");
+        //Double height = scanner.nextDouble();
         
         Double bmi = weight / (height * height);
-        System.out.println("Hello " + name);
-        System.out.println("Your BMI is: " + Math.round(bmi));
+        IO.writeln("Your BMI is: " + IO.round(bmi, 2));
         
-        scanner.close();
+        //scanner.close();
     }
     
 }
