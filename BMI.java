cbc9481d36b2ab0024b69727aa32c13b75ec6d38
Helge <helgepeggen@web.de>
2024-07-01 09:08:12 +0200

    Delete scanner

--- BMI.java
+++ BMI.java
@@ -3,23 +3,17 @@
 public class BMI {
 
     public static void main(String[] args) {
-		
-        //Scanner scanner = new Scanner(System.in);
 
         String name = IO.promptAndReadString("What's your name? ");
-        //String name = scanner.next();
         IO.writeln("Hello " + name);
         
         Double weight = IO.promptAndReadDouble("What's your weight in kilogram? ");
-        //Double weight = scanner.nextDouble();  
 
         Double height = IO.promptAndReadDouble("What's your height in meter? ");
-        //Double height = scanner.nextDouble();
         
         Double bmi = weight / (height * height);
         IO.writeln("Your BMI is: " + IO.round(bmi, 2));
         
-        //scanner.close();
     }
     
 }
