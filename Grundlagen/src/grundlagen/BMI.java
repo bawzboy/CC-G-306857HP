5763afdae61833de1eff13100f6f6b115ad66f16
philippetamla <philippe.tamla@incontec.de>
2024-07-01 09:30:35 +0200

    Add solution 2 for BMI using IO class

--- Grundlagen/src/grundlagen/BMI.java
+++ Grundlagen/src/grundlagen/BMI.java
@@ -10,16 +10,16 @@ public class BMI {
 		
 		System.out.println("What's your name?");
 		String name = scanner.next();
-		System.out.println("Name: " + name);
+		// System.out.println("Name: " + name);
 
 		System.out.println("What's your weight?");
 		double weight = scanner.nextDouble();
-		System.out.println("Weight: " + weight);
+		// System.out.println("Weight: " + weight);
 		
 		
 		System.out.println("What's your height?");
 		double height = scanner.nextDouble();
-		System.out.println("Height: " + height);
+		// System.out.println("Height: " + height);
 		
 		double bmi = weight / (height * height);
 		System.out.print("Hello " + name);
