e5dcfacbd7d687b35841c3cf91be19154325c720
Helge <helgepeggen@web.de>
2024-07-02 18:47:17 +0200

    -

---
+++ Char.java
@@ -0,0 +1,107 @@
+public class Char {
+    public static void main(String[] args) {
+        
+        //Aufgabe 2.5 / 1
+
+        // int h = (int) 'H';
+        // int e = (int) 'e';
+        // int l = (int) 'l';
+        // int o = (int) 'o';
+        // int space = (int) ' ';
+        // int W = (int) 'W';
+        // int r = (int) 'r';
+        // int d = (int) 'd';
+        
+        // System.out.println("ASCII value of H is: " + 'H');
+        // System.out.println("ASCII value of e is: " + 'e');
+        // System.out.println("ASCII value of l is: " + 'l');
+        // System.out.println("ASCII value of l is: " + 'l');
+        // System.out.println("ASCII value of o is: " + 'o');
+        // System.out.println("ASCII value of 'space' is: " + ' ');
+        // System.out.println("ASCII value of W is: " + 'W');
+        // System.out.println("ASCII value of o is: " + 'o');
+        // System.out.println("ASCII value of r is: " + 'r');
+        // System.out.println("ASCII value of l is: " + 'l');
+        // System.out.println("ASCII value of d is: " + 'd');
+        
+        // System.out.println("ASCII value of H is: " + (int) 'H');
+        // System.out.println("ASCII value of e is: " + (int) 'e');
+        // System.out.println("ASCII value of l is: " + (int) 'l');
+        // System.out.println("ASCII value of l is: " + (int) 'l');
+        // System.out.println("ASCII value of o is: " + (int) 'o');
+        // System.out.println("ASCII value of 'space' is: " + (int) ' ');
+        // System.out.println("ASCII value of W is: " + (int) 'W');
+        // System.out.println("ASCII value of o is: " + (int) 'o');
+        // System.out.println("ASCII value of r is: " + (int) 'r');
+        // System.out.println("ASCII value of l is: " + (int) 'l');
+        // System.out.println("ASCII value of d is: " + (int) 'd');
+        
+        // Aufgabe 2.5 / 2
+
+        // char [] Text = {'H', 'e', 'l', 'l', 'o', ' ', 'W', 'o', 'r', 'l', 'd'};
+        // String Text = "Hello World";
+        
+        // System.out.println((int) Text.charAt(0));
+        // System.out.println((int) Text.charAt(1));
+        // System.out.println((int) Text.charAt(2));
+        // System.out.println((int) Text.charAt(3));
+        // System.out.println((int) Text.charAt(4));
+        // System.out.println((int) Text.charAt(5));
+        // System.out.println((int) Text.charAt(6));
+        // System.out.println((int) Text.charAt(7));
+        // System.out.println((int) Text.charAt(8));
+        // System.out.println((int) Text.charAt(9));
+        // System.out.println((int) Text.charAt(10));
+
+        // Aufgabe 2.5 / 3
+
+        // System.out.println("Zeichen \t" + "ASCII-Wert");
+        // System.out.println("H \t\t" + (int) Text.charAt(0));
+        // System.out.println("e \t\t" + (int) Text.charAt(1));
+        // System.out.println("l \t\t" + (int) Text.charAt(2));
+        // System.out.println("l \t\t" + (int) Text.charAt(3));
+        // System.out.println("o \t\t" + (int) Text.charAt(4));
+        // System.out.println("'space' \t" + (int) Text.charAt(5));
+        // System.out.println("W \t\t" + (int) Text.charAt(6));
+        // System.out.println("o \t\t" + (int) Text.charAt(7));
+        // System.out.println("r \t\t" + (int) Text.charAt(8));
+        // System.out.println("l \t\t" + (int) Text.charAt(9));
+        // System.out.println("d \t\t" + (int) Text.charAt(10));
+
+        // Bonus for - Schleife
+        
+        // for (int i = 0; i < Text.length; i++){
+        //     System.out.println(Text[i] + "\t" + (int) Text[i]);
+        // }
+
+        // Aufgabe 2.6
+
+        // int i = 0; int n = i++;
+        // System.out.println("A : i = " + i + " n = " + n);
+        // Ergebnis: i = 1, n = 0
+
+        // int i = 10; int n = ++i;
+        // System.out.println("B : i = " + i + " n = " + n);
+        // Ergebnis: i = 11, n = 11
+
+        // int i = 20; int j = 5; int n = i++; ++j; 
+        // System.out.println("C : i = " + i + " j = " + j + " n = " + n);
+        // Ergebnis: i = 21, j = 6, n = 20
+
+        // int i = 15; int n = i += 3;
+        // System.out.println("D : i = " + i + " n = " + n);
+        // Ergebnis: i = 18, n = 18
+
+        // int i = 3; int j = 5; int n = i *= --j;
+        // System.out.println("E : i = " + i + " j = " + j + " n = " + n);
+        // Ergebnis: i = 12, j = 4, n = 12
+    
+        // Aufgabe 2.7
+
+        // double number = IO.promptAndReadDouble("Your number: ");
+        // double roundedNumber = (int) (number * 100.0 + 0.5) / 100.0 ;
+        // System.out.println("Your number rounded: " + roundedNumber);
+
+
+    }
+}
