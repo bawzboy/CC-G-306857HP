95e03d1f9b048ce5b5d2997c81f33eeb1bdf6bc0
Helge <helgepeggen@web.de>
2024-07-09 08:56:28 +0200

    -

--- Arrays/EinfacheProgrammierungMitArrays.java
+++ Arrays/EinfacheProgrammierungMitArrays.java
@@ -5,38 +5,39 @@ import java.util.Arrays;
 public class EinfacheProgrammierungMitArrays {
     public static void main(String[] args) {
         // 1.
-        // Integer [] zahlen = new Integer [5];
+        Integer [] zahlen = new Integer [5];
         
-		// for (int i = 0; i < zahlen.length; i++) {
-		// 	zahlen[i] = (int)(Math.random() * 10);
-		// }
+		for (int i = 0; i < zahlen.length; i++) {
+			zahlen[i] = (int)(Math.random() * 10);
+		}
         
-        // int ergebnis = 0;
-        // for (int value = 0; value < zahlen.length; value ++) {
-        //     System.out.println(zahlen[value]);
-        //     ergebnis = ergebnis + zahlen[value]; 
-        // }
+        int ergebnis = 0;
+        for (int value = 0; value < zahlen.length; value ++) {
+            System.out.println(zahlen[value]);
+            ergebnis = ergebnis + zahlen[value]; 
+        }
 
-        // System.out.println(ergebnis);
+        System.out.println(Arrays.toString(zahlen));
+        System.out.println(ergebnis);
         
         // 2.
-        Integer [] arrayOne = new Integer [20];
-        Integer [] arrayTwo = new Integer [20];
+        // Integer [] arrayOne = new Integer [20];
+        // Integer [] arrayTwo = new Integer [20];
 
-        for (int i = 0; i < arrayOne.length; i ++) {
-            int randNo = (int) (Math.random() * 50) * 2;
-            arrayOne[i] = randNo;
-        }
+        // for (int i = 0; i < arrayOne.length; i ++) {
+        //     int randNo = (int) (Math.random() * 50) * 2;
+        //     arrayOne[i] = randNo;
+        // }
 
-        System.out.println(Arrays.toString(arrayOne));
+        // System.out.println(Arrays.toString(arrayOne));
 
-        for (int i = 0; i < arrayTwo.length; i ++) {
-            arrayTwo[i] = (int) (Math.random() * 100);
-        }
+        // for (int i = 0; i < arrayTwo.length; i ++) {
+        //     arrayTwo[i] = (int) (Math.random() * 100);
+        // }
 
-        Arrays.sort(arrayTwo);
+        // Arrays.sort(arrayTwo);
 
-        System.out.println(Arrays.toString(arrayTwo));
+        // System.out.println(Arrays.toString(arrayTwo));
        
         
     }
