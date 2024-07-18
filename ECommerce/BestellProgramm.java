60dff1092214c7ab66f4336a5c7311459241985e
Helge <helgepeggen@web.de>
2024-07-18 11:06:04 +0200

    -

--- ECommerce/BestellProgramm.java
+++ ECommerce/BestellProgramm.java
@@ -34,9 +34,9 @@ public class BestellProgramm {
 
         testWarenkorb.addItem(itemEins);
         testWarenkorb.addItem(itemZwei);
+        testWarenkorb.setGesamtbetrag(testWarenkorb.gesamtbetrag(testWarenkorb.warenkorbItems));
         System.out.println(testWarenkorb);
         
-        // testWarenkorb.setGesamtbetrag(testWarenkorb.gesamtbetrag(testWarenkorb.warenkorbItems));
         
     }
 }
