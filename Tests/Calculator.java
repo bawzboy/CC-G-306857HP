8a0ca90b27fb2c71208fabe3e62fb78ba2669d90
Helge <helgepeggen@web.de>
2024-07-24 12:48:33 +0200

    -

---
+++ Tests/Calculator.java
@@ -0,0 +1,20 @@
+package Tests;
+
+public class Calculator {
+    
+    public static int add(int a, int b) {
+        return a + b;
+    }
+
+    public static int subtract(int a, int b) {
+        return a - b;
+    }
+
+    public static int multiply(int a, int b) {
+        return a * b;
+    }
+
+    public static int divide(int a, int b) {
+        return a / b;
+    }
+}
---
+++ Tests/CalculatorTest.java
@@ -0,0 +1,40 @@
+package Tests;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class CalculatorTest {
+    
+    @Test
+	public void testAddPositiveNumbers() {
+        assertEquals(5, Calculator.add(2, 3));
+        assertEquals(100, Calculator.add(50, 50));
+    }
+
+    @Test
+	public void testAddNegativeNumbers() {
+        assertEquals(-5, Calculator.add(-2, -3));
+        assertEquals(-100, Calculator.add(-50, -50));
+    }
+
+    @Test
+	public void testAddPositiveAndNegativeNumbers() {
+        assertEquals(1, Calculator.add(-2, 3));
+        assertEquals(-1, Calculator.add(2, -3));
+    }
+
+    @Test
+	public void testAddWithZeros() {
+        assertEquals(2, Calculator.add(2, 0));
+        assertEquals(0, Calculator.add(0, 0));
+        assertEquals(-2, Calculator.add(0, -2));
+    }
+
+    @Test
+	public void testAddWithBoundaryValues() {
+        assertEquals((double) Integer.MAX_VALUE + 1, Calculator.add(Integer.MAX_VALUE, 1), 0.0001);
+        assertEquals((double) Integer.MIN_VALUE - 1, Calculator.add(Integer.MIN_VALUE, -1), 0.0001);
+    }
+
+}
